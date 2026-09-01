5841e2e0cd59fa9857c3735f8a255172d2dddf7b
liorZahavi <lior.zahavi@stu.shenkar.ac.il>
2026-09-01 15:25:45 +0300

    adding the file that help deploy to github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from './auth/AuthContext'
 import App from './App.jsx'
 import './i18n'
@@ -8,10 +8,10 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <AuthProvider>
     <App />
     </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
